fa2f4bd52c84260c36f90065bff3090caf0e9e23
jorge <91345902+jreyesgfi@users.noreply.github.com>
2021-12-08 14:37:07 +0100

    Ajustes para representar

--- pruebas.py
+++ pruebas.py
@@ -1,16 +1,25 @@
-import treap_structure as treap
+# import treap_structure as treap
 
-tree = treap.GrumoTreap()
-primerHijo = treap.Usuario(0)
-otroHijo = treap.Usuario(19)
-primerHijo.hijoMayor = otroHijo
-tree.___init__(primerHijo)
-tree.addRelacion(6,2)
-tree.addRelacion(2,4)
-tree.addRelacion(5,3)
-tree.addRelacion(3,7)
-tree.addRelacion(7,4)
-tree.addRelacion(2,1)
-print("###########################")
-for hijo in tree.hijos:
-    print(tree.representarEnCascada(hijo))
+# tree = treap.GrumoTreap()
+# primerHijo = treap.Usuario(0)
+# otroHijo = treap.Usuario(19)
+# primerHijo.hijoMayor = otroHijo
+# tree.___init__(primerHijo)
+# tree.addRelacion(6,2)
+# tree.addRelacion(2,4)
+# tree.addRelacion(5,3)
+# tree.addRelacion(3,7)
+# tree.addRelacion(7,4)
+# tree.addRelacion(2,1)
+# print("###########################")
+# for hijo in tree.hijos:
+#     print(tree.representarEnCascada(hijo))
+
+import treap_structure_final as treap
+
+bosque = treap.Bosque()
+bosque.___init__()
+for parejaNumero in [(5,2),(10,3),(21,15),(4,2),(5,7),(8,10)]:
+    bosque.addConexion(parejaNumero[0],parejaNumero[1])
+bosque.crearGrumos(bosque.conexiones)
+print(bosque.representarGrumos())
--- treap_structure_final.py
+++ treap_structure_final.py
@@ -1,24 +1,23 @@
 class NodoArbol:
-    def __init__(self, id):
+    def __init__(self, id = None):
         self.id = id
         self.hijoMayor = None
         self.hijoMenor = None
         self.relaciones = []
         self.nuevo = True
 
-    def addRelacion(self,usrToFind,usrAmigo):
-        usr = self.addUsuario(self,usrToFind)
-        usr.relaciones.append(usrAmigo)
-
     def addUsuario(self,id):
+        if self.id == None:
+            self.id = id
+            return self
         if self.id > id:
             if self.hijoMenor:
-                return self.addUsuario(self.hijoMenor,id)
+                return self.hijoMenor.addUsuario(id)
             self.hijoMenor = NodoArbol(id)
             return self.hijoMenor
         if self.id < id:
             if self.hijoMayor:
-                return self.addUsuario(self.hijoMayor,id)
+                return self.hijoMayor.addUsuario(id)
             self.hijoMayor = NodoArbol(id)
             return self.hijoMayor
         #   Indicamos que el nodo ya existía
@@ -26,30 +25,34 @@ class NodoArbol:
         return self
 
 
-class ArbolConexiones(NodoArbol):
-    def ___init__(self,rel):
+class Bosque(NodoArbol):
+    def ___init__(self):
         #   Llamamos a la clase padre
         super().__init__()
 
         #   Definimos las raices de nuestros tres árboles
-        self.assig = None
-        self.conexiones = None
-        self.grumos = None
+        self.assig = NodoArbol()
+        self.conexiones = NodoArbol()
+        self.grumos = []
 
     def addConexion(self,id1, id2):
         #  Añadimos el usuario1 al árbol de conexiones y el usuario2 a sus relaciones
         usuario1 = self.conexiones.addUsuario(id1)
         usuario1.relaciones.append(id2)
 
-    def construirGrumos(self,raizGrumo,usuarioConex):
+        #   También lo hacemos al revés
+        usuario2 = self.conexiones.addUsuario(id2)
+        usuario2.relaciones.append(id1)
+
+    def construirGrumos(self,raizGrumo,id):
 
         # comprobar si usuario esta asig y si no lo añadimos
-        usuarioAssig = self.assig.addUsuario(usuarioConex.id)
+        usuarioAssig = self.assig.addUsuario(id)
         if usuarioAssig.nuevo:
 
             #   Añadimos el usuario al árbol del grumo particular
-            raizGrumo.addUsuario(usuarioConex.id)
-            for hijo in usuarioConex.relaciones:
+            raizGrumo.addUsuario(id)
+            for hijo in self.conexiones.addUsuario(id).relaciones:
                 self.construirGrumos(raizGrumo,hijo)
     
     def crearGrumos(self, raizGrumoConex):
@@ -59,12 +62,25 @@ class ArbolConexiones(NodoArbol):
         if usuarioAssig.nuevo:
 
             #   Si no estaba creamos un nuevo grumo
-            raizGrumo = self.grumos.addUsuario(raizGrumoConex.id)
+            raizGrumo = NodoArbol(raizGrumoConex.id)
+            self.grumos.append(raizGrumo)
             for hijo in raizGrumoConex.relaciones:
                 self.construirGrumos(raizGrumo,hijo)
 
         #   A continuación seguimos descendiendo en el árbol de conexiones
         if raizGrumoConex.hijoMenor:
             self.crearGrumos(raizGrumoConex.hijoMenor)
-        if raizGrumo.hijoMayor:
+        if raizGrumoConex.hijoMayor:
             self.crearGrumos(raizGrumoConex.hijoMayor)
+    def representarGrumos(self):
+        for grumo in self.grumos:
+            self.representarEnCascada(grumo)
+    def representarEnCascada(self, elemento,espacios=0,iteraciones=20):
+        espacios += 1
+        iteraciones -= 1
+        cadena= "-"
+        if elemento and iteraciones>0:
+            espacios += 3
+            cadena = "{}->|{} \n{}|{}".format(elemento.id,self.representarEnCascada(elemento.hijoMenor,espacios,iteraciones),(" "*espacios),self.representarEnCascada(elemento.hijoMayor,espacios,iteraciones))
+        
+        return cadena
