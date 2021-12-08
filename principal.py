0b655df6627e720832ce53d53f327945233f497d
jorge <91345902+jreyesgfi@users.noreply.github.com>
2021-12-08 21:19:36 +0100

    Verisión acabada provisional

--- principal.py
+++ principal.py
@@ -7,7 +7,7 @@ bosque.___init__()
 
 
 #Empezamos leyendo las líneas del documento una a una
-def leer_relaciones(fprin):
+def leer_relaciones(fprin, fextra):
 
     #   Leemos el fichero y modificamos nuestro registro
     with open(fprin) as f:
@@ -25,47 +25,39 @@ def leer_relaciones(fprin):
             l = l + 1
 
 
-    # if fextra != "":
-    #     with open(fextra) as f:
-    #         for linea in f:
-    #             nr = []
-    #             for palabra in linea.split():
-    #                 nr.append(int(palabra))
-    #                 '''try:
-    #                     nr.append(int(palabra))
-    #                 except:
-    #                     print("La relación de una pareja de elementos es incorrecta")'''
-    #             if (len(nr) == 2):
-    #                 red.append(nr)
-    # return (red)
+    if fextra != "":
+        with open(fextra) as f:
+            for linea in f:
+                #   Definimos una nueva relación
+                pareja = linea.split()
+                bosque.addConexion(pareja[0],pareja[1])
 
-def seleccion_grumos(bosque, porcen):
-    grumosSeleccionados = bosque.obtenerPorcentajes(porcen)
+def salvar_nuevas_relaciones(grumosSeleccionados):
+    print('Guardando nueva relación...')
+
     #  Salvamos las nuevas relaciones
     f = open("extra.txt","w") #De esta forma se borra automáticamente el anterior extra.txt
     grumoPasado = None
+
     for grumo in grumosSeleccionados:
+
         # Comprobamos que no estamos en el primer grupo
         if grumoPasado != None:
             # Proponemos la nueva relación
             f.write(grumo['raiz'].id+" "+grumoPasado['raiz'].id+"\n")
+
         # Definimos el nuevo grumo
         grumoPasado = grumo
-        
-            
-
-
-    return grumosSeleccionados
 
 def main():
     # Preguntar fichero a abrir y porcentaje
-    principal = input("Introduzca el nombre del fichero principal:\n")
-    #   extra = input("Introduzca el nombre del fichero extra o pulse ENTER.")
-    porcen = float(input("Introduzca el porcentaje deseado:\n")) / 100
+    principal = input("Introduzca el nombre del fichero principal:  ")
+    extra = input("Introduzca el nombre del fichero extra o pulse ENTER:  ")
+    porcen = float(input("Introduzca el porcentaje deseado:  ")) / 100
 
     # Leemos el documento y construimos el árbol conexión
     tiempo1 = time.time()
-    leer_relaciones(principal)
+    leer_relaciones(principal, extra)
     t_leer_doc = time.time() - tiempo1
 
     # Construimos los grumos como árboles
@@ -75,7 +67,9 @@ def main():
 
     # Selección de grumos
     tiempo1=time.time()
-    grumosSeleccionados = seleccion_grumos(bosque, porcen)
+    grumosSeleccionados = bosque.obtenerPorcentajes(porcen)
+    if extra =='' :
+        salvar_nuevas_relaciones(grumosSeleccionados)
     t_seleccion_grumos = time.time() - tiempo1
 
 
@@ -98,7 +92,7 @@ def main():
     i = 0
     for grumo in grumosSeleccionados:
         i += 1
-        print("El porcentaje del grumo {} es {:.2f}".format(i,grumo['porcentaje']))
+        print("El porcentaje del grumo {} es {:.2f}%".format (i, grumo['porcentaje']*100 ) )
 
 
     #   Recomendaciones de uniones
